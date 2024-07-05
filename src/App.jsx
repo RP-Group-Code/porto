7a2faca241700280abc6f40e36500824108288fa
RP-Group-Code <mhasurungan54@gmail.com>
2024-07-05 17:56:30 +0700

    add link porto

--- src/App.jsx
+++ src/App.jsx
@@ -16,25 +16,11 @@ const App = () => {
       {/* <Home /> */}
 
       <Routes>
-        <Route path="/" exact element={<Home />} />
+        <Route path="/porto" exact element={<Home />} />
         <Route path="/tekno" element={<Tekno />} />
-        {/* <Route path="/about" exact element={<About />} />
-        <Route path="/header" exact element={<Header />} />
-        <Route path="/nav" exact element={<Nav />} />
-        <Route path="/exc" exact element={<Experience />} />
-        <Route path="/ser" exact element={<Services />} />
-        <Route path="/porto" exact element={<Porto />} />
-        <Route path="/con" exact element={<Contact />} />
-        <Route path="/fot" exact element={<Footer />} /> */}
+       
       </Routes>
-      {/* <Nav />
-      <Header />
-      <About />
-      <Experience />
-      <Services />
-      <Porto />
-      <Contact />
-      <Footer /> */}
+     
     </BrowserRouter>
   );
 };
